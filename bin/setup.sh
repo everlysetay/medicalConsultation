d872cbee7a57c9530335494461752e2daf8912b9
Everlyse Tay <Everlyse.Tay@gen-net.com.sg>
2020-01-11 23:51:25 +0800

    add setup script

---
+++ bin/setup.sh
@@ -0,0 +1,18 @@
+#Add script to:
+# * Install dependencies
+# * Build/Compile
+# * Run Test Suit to validate
+#
+# After this is run, jar file
+# this should work
+
+#Install Dependencies
+#/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+#brew install gradle
+
+#Build/Compile
+grade assemble
+grade build
+
+#Run Junit Tests
+gradle test
